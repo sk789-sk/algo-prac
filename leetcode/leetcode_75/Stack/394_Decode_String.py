d8ab30135c5b83731a65195b4a16470d52e619c4
Shams <skm478@gmail.com>
2024-02-28 14:26:57 -0500

    394 done

--- leetcode/leetcode_75/Stack/394_Decode_String.py
+++ leetcode/leetcode_75/Stack/394_Decode_String.py
@@ -6,33 +6,57 @@
 
 # The test cases are generated so that the length of the output will never exceed 105. -->
 
-def solution(s):
-    out_str = ''
-    
-    stack = [] 
-    encoded_str = ''
 
+#approach is we keep it all within the stack. and at the end the stack will represent the string
+#append characters from the string to the stack until we encounter a ]. 
+#Once we encounter that we pop characters until we hit a [. This sequence represents the string that may be multiplied. 
+#we then pop the characters after the [ which will be digits until we see that the preceding value is not a digit.
+#We then multiply the number with the encoded string and then append that to the stack. 
+#We then continue down the original string. 
+#Once the string is fully traversed we have only characters in the stack representing the string after it has been decoded.
+#Turn the stack into a string and then return it.  
+
+def solution(s):
+    stack = []
     for val in s:
-        #we only need to start popping and creating the string once we hit a ] character
         if val != ']':
             stack.append(val)
-        else:
-            while stack:
-                char = stack.pop()
-                if char.isalpha():
-                    #push to front of the encoded string 
-                    pass
-                elif char.isdigit():
-                    #multiply the encoded_str by the digit
-                    pass
-        
-        out_str += encoded_str #add this to the end string when the stack is empty. 
-            #once stack is empty we add the encoded_str to the out_str
-        
+            print(stack)
+        else: #we have encountered ]
+            #we pop until the first [ and find the multiples of it. expand the string and then add that to the stack.
+            encoded_str = ''
+            char = stack.pop()
 
+            while char != '[':
+                #we pop    
+                if char.isalpha():
+                    encoded_str = char + encoded_str
+                    print(encoded_str)
+                char = stack.pop()         
+
+            if char == '[':
+                print('la')
+                num = stack.pop()
+                while stack and stack[-1].isdigit():
+                    num = stack[-1] + num
+                    stack.pop()
+
+                full_str = int(num) * encoded_str
+                print(full_str)
+                for char in full_str:
+                    stack.append(char)
+
+    print(stack)
+    out_str = "".join(stack)
+    print(out_str)
+    return(out_str)
 
 
 t1 = '3[a]2[bc]'
-t2 = "3[a2[c]]"
+t1 = "3[a2[c]]"
+t1 = "2[abc]3[cd]ef"
+t2 = "100[lee]"
+t1 = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef"
+t1 = 'abcd'
+
 solution(t1)
-solution(t2)
